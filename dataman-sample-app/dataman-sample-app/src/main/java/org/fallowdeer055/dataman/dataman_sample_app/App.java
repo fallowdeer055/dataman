5f5019b44edda9e2d4165aaca3204cac2594af34
fallowdeer055 <fallowdeer055@gmail.com>
2018-02-06 12:15:15 +0100

    sample-app prototype

---
+++ dataman-sample-app/dataman-sample-app/src/main/java/org/fallowdeer055/dataman/dataman_sample_app/App.java
@@ -0,0 +1,13 @@
+package org.fallowdeer055.dataman.dataman_sample_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
