cbe90ed133362d6bec759a653327603dc3002312
fallowdeer055 <fallowdeer055@gmail.com>
2018-02-06 12:19:35 +0100

    api first thoughts

---
+++ dataman-lib/src/main/java/org/fallowdeer055/dataman/api/EntityList.java
@@ -0,0 +1,5 @@
+package org.fallowdeer055.dataman.api;
+
+public interface EntityList {
+
+}
---
+++ dataman-lib/src/main/java/org/fallowdeer055/dataman/impl/EntityListImpl.java
@@ -0,0 +1,7 @@
+package org.fallowdeer055.dataman.impl;
+
+import org.fallowdeer055.dataman.api.EntityList;
+
+public class EntityListImpl implements EntityList {
+
+}
